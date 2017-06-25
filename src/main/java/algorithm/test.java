f229f636989baa79cd0ca5c4302c047cdcb10a6b
liruiqi123 <1595854626@qq.com>
2017-06-25 09:28:58 +0800

    开始你的表演

--- src/main/java/algorithm/test.java
+++ src/main/java/algorithm/test.java
@@ -6,5 +6,6 @@ package algorithm;
 public class test {
     public  static  void main(String args[]){
         System.out.println("恭喜你，拿到一血！！！");
+        System.out.println("请开始你的表演！！！");
     }
 }
