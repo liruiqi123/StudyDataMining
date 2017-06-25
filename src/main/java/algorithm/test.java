f9f7c839a87f47bf2339d39fb0c6a3a47bf9d799
liruiqi123 <1595854626@qq.com>
2017-06-25 09:27:28 +0800

    开始你的表演

---
+++ src/main/java/algorithm/test.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+/**
+ * Created by liruiqi on 2017/6/25.
+ */
+public class test {
+    public  static  void main(String args[]){
+        System.out.println("恭喜你，拿到一血！！！");
+    }
+}
